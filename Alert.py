8ee5c4687999c22a17294ef753db2fbc1bd01c5b
smiley042894 <ian.nytes@gmail.com>
2021-02-24 01:04:48 -0600

    Add files via upload

---
+++ Alert.py
@@ -0,0 +1,30 @@
+import gspread
+import schedule
+import smtplib
+import os
+
+
+
+gc = gspread.service_account(filename='credentials.json')
+sh = gc.open_by_key('1DeAFaS_fHhnGIdCDdZBzZcvmvyx-ycy0dyIOvyn96VE')
+
+
+#Send and email using smtplib through gmail
+def send_alert(recipient, subject, msg):
+    
+    #sets up access to mailserver base on the user info located in .zshrc or whatever the defaul shell is
+    email_username = os.environ.get('email_username')
+    email_password = os.environ.get('email_python_password')
+    
+    server = smtplib.SMTP('smtp.gmail.com')
+    server.connect('smtp.gmail.com', '587')
+    server.ehlo()
+    server.starttls()
+    server.login(email_username, email_password)
+  
+    #  formats the email to be sent properly by .sendmail
+    email = f'Subject: {subject}\n\n{msg}'
+
+    server.sendmail(email_username, recipient, email)
+
+send_alert('2626234965@email.uscc.net', 'Record Your Calories', 'Do it, or I\'ll murder you')        
